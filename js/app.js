2b52dfe03f738ddd361e13d168a03c9edbb1149e
natalia <nataliav@gmail.com>
2015-10-01 11:58:10 +0200

    Increse clicks when clicking image on display cat

--- js/app.js
+++ js/app.js
@@ -1,25 +1,26 @@
 $( document ).ready(function() {
-    $( ".kittens" ).delegate( ".kitten", "click", increaseClicks);
+    $( ".kittens" ).delegate( "h3", "click", displayCat);
 
+    $( ".cat-display" ).delegate( "img", "click", increaseClicks);
 
-    function displayClicks(num, element){
-      element.text(num);
-    }
-
-    function displayCat(element){
-      console.log(element.children('h3').text(), element);
-      var display = $('#cat-display');
+    function displayCat(){
+      var element = $(this).parent();
+      var display = $('.cat-display');
       var cat = '<h3>' + element.children('h3').text()+ '</h3>'+
-          '<img src="'+ element.children('img').attr('src') +'" alt="Cute kitten" class="kitten">'+
+          '<img src="img/'+ element.data('img') +'.jpg" alt="Cute kitten" class="kitten">'+
           '<div class="clicks">'+ element.children('.clicks').text() + '</div>'
       display.html(cat);
+      display.attr('id', element.data('id')) ;
 
     }
 
     function increaseClicks(){
-      var clicksElement = $(this).siblings('.clicks');
-      var clicks = parseInt(clicksElement.text()) +1;
-      displayClicks(clicks, clicksElement);
-      displayCat($(this).parent());
+      var display = $(this).parent();
+      var id = display.attr('id');
+      var clicksElement = $(".kittens").find("[data-id='" + id + "']");
+      var clicks = parseInt(clicksElement.data('clicks')) +1;
+      clicksElement.data('clicks', clicks);
+      display.children('.clicks').text(clicks);
+
     }
 });
