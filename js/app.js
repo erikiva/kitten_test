251eeac0b9a5d3419b99b8f9387a7c15e8a5c02e
natalia <nataliav@gmail.com>
2015-10-01 11:11:13 +0200

    Added display area

--- js/app.js
+++ js/app.js
@@ -6,10 +6,20 @@ $( document ).ready(function() {
       element.text(num);
     }
 
+    function displayCat(element){
+      console.log(element.children('h3').text(), element);
+      var display = $('#cat-display');
+      var cat = '<h3>' + element.children('h3').text()+ '</h3>'+
+          '<img src="'+ element.children('img').attr('src') +'" alt="Cute kitten" class="kitten">'+
+          '<div class="clicks">'+ element.children('.clicks').text() + '</div>'
+      display.html(cat);
+
+    }
+
     function increaseClicks(){
       var clicksElement = $(this).siblings('.clicks');
       var clicks = parseInt(clicksElement.text()) +1;
       displayClicks(clicks, clicksElement);
-
+      displayCat($(this).parent());
     }
 });
