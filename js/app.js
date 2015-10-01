268c621ed008faa49a8b28d55c77e6c69edae02c
natalia <nataliav@gmail.com>
2015-10-01 13:15:03 +0200

    added admin

--- js/app.js
+++ js/app.js
@@ -1,10 +1,17 @@
 $( document ).ready(function() {
-    $( ".kittens" ).delegate( "h3", "click", displayCat);
 
+    $( ".kittens" ).delegate( "h3", "click", display);
     $( ".cat-display" ).delegate( "img", "click", increaseClicks);
+    $("#admin-area").delegate("button#cancel", "click", hideAdmin);
+    $("#admin-area").delegate("form", "submit", saveData);
 
-    function displayCat(){
-      var element = $(this).parent();
+
+    function display(){
+        var element = $(this).parent();
+        displayCat(element);
+    }
+
+    function displayCat(element){
       var display = $('.cat-display');
       var cat = '<h3>' + element.children('h3').text()+ '</h3>'+
           '<img src="img/'+ element.data('img') +'.jpg" alt="Cute kitten" class="kitten">'+
@@ -23,4 +30,40 @@ $( document ).ready(function() {
       display.children('.clicks').text(clicks);
 
     }
+
+    $('#show-admin').on('click', loadAdmin);
+
+    function loadAdmin(){
+
+      $('#admin-area').load('admin.html', loadFormData);
+
+    }
+
+    function loadFormData(){
+        var cat_id = $('.cat-display').attr('id');
+      if (!cat_id){
+          cat_id = 1;
+      }
+      var selected_cat = $(".kittens").find("[data-id='" + cat_id + "']");
+      $("#admin input[name='name']").val(selected_cat.children('h3').text());
+      $("#admin input[name='clicks']").val(selected_cat.data('clicks'));
+      $("#admin input[name='id']").val(cat_id);
+    }
+
+    function saveData(ev){
+      ev.preventDefault();
+      var selected_cat = $(".kittens").find("[data-id='" + $("#admin input[name='id']").val() + "']");
+      selected_cat.data('clicks', $("#admin input[name='clicks']").val());
+      selected_cat.children('h3').text($("#admin input[name='name']").val());
+      displayCat(selected_cat);
+      hideAdmin();
+    }
+
+    function hideAdmin(ev){
+       ev.preventDefault();
+      $('#admin-area').text("");
+    }
+
+
+
 });
